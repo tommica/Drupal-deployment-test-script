ed0df2173f0a54e79fce3cc8d5f33ce01c48733c
Tommi Carleman <me@tommcarleman.net>
2012-09-01 15:20:27 +0200

    update

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Drupal-deployment-test-script
 
 Script for deployment + makefile
 
-Run this from the Drupal root folder
+Run this from the Drupal root folder (where you want to install drupal)
 
 Please check that you have installed:
 
@@ -12,4 +12,4 @@ Git
 
 Usage:
 
-./tommidep.sh "mysql://user:pass/server:port/dbname" "drupal_username" "drupal_password"
+wget https://raw.github.com/tommica/Drupal-deployment-test-script/master/tommidep.sh && chmod +x tommidep.sh && ./tommidep.sh "mysql://user:pass/server:port/dbname" "drupal_username" "drupal_password" "sitename" && rm tommidep.sh
--- tommidep.sh
+++ tommidep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if ! type "drush" > /dev/null; then
+if ! type "drush" &> /dev/null; then
   echo "$(tput setb 0) $(tput setaf 1) PLEASE INSTALL DRUSH 5+ $(tput sgr0)"
   exit 0
 fi
@@ -10,9 +10,24 @@ if ! type "git" &> /dev/null; then
 fi
 
 echo "$(tput setb 1) Grab an energy drink, this might take a while "
-echo $1
-echo $2
-echo $3
 
-echo "$(tput sgr0)"
+wget https://raw.github.com/tommica/Drupal-deployment-test-script/master/tommi.make &&
+drush make -y tommi.make &&
+git clone git@github.com:tommica/Drupal-deployment-test-theme.git sites/all/themes/tommi &&
+git clone git@github.com:tommica/Drupal-deployment-test-profile.git profiles/tommidep &&
+wget https://github.com/tommica/Drupal-deployment-test-features/raw/master/everything_tommidep.tar -O sites/all/modules/everything_tommidep.tar &&
+tar -xvf sites/all/modules/everything_tommidep.tar &&
+drush si -y tommidep --db-url=$1 --account-name=$2 --account-pass=$3 &&
+drush en everything_tommidep &&
+wget http://lorempixel.com/900/900/animals/ -O sites/default/files/bunny-1.jpg &&
+wget http://lorempixel.com/900/900/animals/ -O sites/default/files/bunny-2.jpg &&
+wget http://lorempixel.com/900/900/animals/ -O sites/default/files/dog-1.png &&
+wget http://lorempixel.com/900/900/animals/ -O sites/default/files/dog-2.png &&
+wget http://lorempixel.com/900/900/animals/ -O sites/default/files/cat-1.jpg &&
+wget http://lorempixel.com/900/900/animals/ -O sites/default/files/cat-2.jpg &&
+wget http://lorempixel.com/900/900/nature/ -O sites/default/files/random-1.jpg &&
+wget http://lorempixel.com/900/900/nature/ -O sites/default/files/random-2.jpg &&
+rm -rf tommi.make profiles/tommidep sites/all/modules/everything_tommidep.tar
+
+echo " DONE $(tput sgr0)"
 exit 0
