88dd75239665289a4077ef11be90e7b49c127b2f
Tommi Carleman <me@tommcarleman.net>
2012-09-01 18:09:22 +0200

    update

--- tommidep.sh
+++ tommidep.sh
@@ -17,12 +17,6 @@ git clone git@github.com:tommica/Drupal-deployment-test-features.git IMPORTDATA 
 git clone git@github.com:tommica/Drupal-deployment-test-theme.git sites/all/themes/tommi &&
 tar -xvf IMPORTDATA/everything_tommidep.tar -C sites/all/modules/ &&
 drush si -y standard --db-url="$1" --account-name="$2" --account-pass="$3" --site-name="$4" &&
-drush en -y ckeditor features &&
-drush ckeditor-download &&
-drush pm-enable -y tommi &&
-drush vset theme_default tommi &&
-drush en -y everything_tommidep &&
-rm -rf tommi.make IMPORTDATA &&
 wget http://lorempixel.com/900/900/animals/ -O sites/default/files/bunny-1.jpg &&
 wget http://lorempixel.com/900/900/animals/ -O sites/default/files/bunny-2.jpg &&
 wget http://lorempixel.com/900/900/animals/ -O sites/default/files/dog-1.png &&
@@ -30,7 +24,13 @@ wget http://lorempixel.com/900/900/animals/ -O sites/default/files/dog-2.png &&
 wget http://lorempixel.com/900/900/animals/ -O sites/default/files/cat-1.jpg &&
 wget http://lorempixel.com/900/900/animals/ -O sites/default/files/cat-2.jpg &&
 wget http://lorempixel.com/900/900/nature/ -O sites/default/files/random-1.jpg &&
-wget http://lorempixel.com/900/900/nature/ -O sites/default/files/random-2.jpg
+wget http://lorempixel.com/900/900/nature/ -O sites/default/files/random-2.jpg &&
+drush en -y ckeditor features &&
+drush ckeditor-download &&
+drush pm-enable -y tommi &&
+drush vset theme_default tommi &&
+drush en -y everything_tommidep &&
+rm -rf tommi.make IMPORTDATA
 
 echo "$(tput setb 1) DONE $(tput sgr0)"
 exit 0
